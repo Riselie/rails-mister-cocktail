e9ba4d6d4bbf6c53acfd71dabbc966d5d3b9bb2a
Rito Sisinni <ritosisinni@yahoo.gr>
2018-02-23 17:30:38 +0000

    Some changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning database...'
+Dose.destroy_all
 Ingredient.destroy_all
 
 puts 'Creating ingredients...'
